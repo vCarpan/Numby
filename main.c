cde2c7a1626edda89dc2b4ed1bd25edecac192b5
vCarpan <126760547+vCarpan@users.noreply.github.com>
2023-11-15 17:22:45 +0100

    Update main.c
    colored some pixels :D

--- main.c
+++ main.c
@@ -1,35 +1,64 @@
 #include <stdio.h>
 #include <windows.h>
 
-
-#define WINDOW_HEIGTH 400
+#define WINDOW_HEIGHT 400
 #define WINDOW_WIDTH 400
-#define BCKGROUND_COLOR 1 //1 WHITE // 2 GREY // 3 BLACK // 9 BLUE
-
+#define BACKGROUND_COLOR 1 // 1 WHITE // 2 GREY // 3 BLACK // 9 BLUE
 
 const char g_szClassName[] = "myWindowClass";
 
-// Step 4: the Window Procedure
+int getRandomNumber(int x, int y) {
+    return rand() % (y - x + 1) + x;
+}
+
+void ColorPixel(HWND hwnd, int x, int y, COLORREF color)
+{
+    HDC hdc = GetDC(hwnd);
+    SetPixel(hdc, x, y, color);
+    ReleaseDC(hwnd, hdc);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch(msg)
+    int mouseX = LOWORD(lParam);
+    int mouseY = HIWORD(lParam);
+    switch (msg)
     {
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
         break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
+    case WM_DESTROY:
+        PostQuitMessage(0);
         break;
-        case WM_LBUTTONDOWN:
-            printf("Left Click! ");
+    case WM_LBUTTONDOWN:
+        {
+            printf("Left Click! X:%d Y:%d\n", mouseX, mouseY);
+            int a = getRandomNumber(0,255);
+            int b = getRandomNumber(0,255);
+            int c = 0;
+            for(int i= 0; i < 100; i= i+1){
+                for(int j= 0; j < 100; j= j+1){
+                    c = getRandomNumber(0,255);
+                    ColorPixel(hwnd, mouseX+i, mouseY+j, RGB(a, b, c));
+                }
+            }
+        }
         break;
-        case WM_RBUTTONDOWN:
-            printf("Right Click! ");
+    case WM_RBUTTONDOWN:
+
+            printf("Right Click! X:%d Y:%d\n", mouseX, mouseY);
+
+            for(int i= 0; i < 100; i= i+1){
+                for(int j= 0; j < 100; j= j+1){
+                    ColorPixel(hwnd, mouseX+i, mouseY+j, RGB(255, 255, 255));
+                }
+            }
         break;
-        case WM_CHAR:
-            printf("%c",wParam);
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
+    case WM_CHAR:
+        printf("%c", wParam);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
@@ -50,7 +79,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     wc.hInstance     = hInstance;
     wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+BCKGROUND_COLOR);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+BACKGROUND_COLOR);
     wc.lpszMenuName  = NULL;
     wc.lpszClassName = g_szClassName;
     wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
@@ -68,7 +97,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
         g_szClassName,
         "GAME TITLE",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGTH,
+        CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT,
         NULL, NULL, hInstance, NULL);
 
     if(hwnd == NULL)
